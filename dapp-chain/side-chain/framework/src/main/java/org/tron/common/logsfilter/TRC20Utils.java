da2973a8712321d6cabba9ae402fc4b88eb3c91f
shenchuanqiang <bill.shen@tron.network>
2021-01-11 14:59:01 +0800

    add owner.

--- dapp-chain/side-chain/framework/src/main/java/org/tron/common/logsfilter/TRC20Utils.java
+++ dapp-chain/side-chain/framework/src/main/java/org/tron/common/logsfilter/TRC20Utils.java
@@ -228,7 +228,7 @@ public class TRC20Utils {
     TriggerSmartContract.Builder build = TriggerSmartContract.newBuilder();
     build.setData(
         ByteString.copyFrom(data));
-    build.setOwnerAddress(ByteString.EMPTY);
+    build.setOwnerAddress(ByteString.copyFrom(Commons.decodeFromBase58Check("T9yD14Nj9j7xAB4dbGeiX9h8unkKHxuWwb")));
     build.setCallValue(0);
     build.setCallTokenValue(0);
     build.setTokenId(0);
